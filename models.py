d21064a82b666fd6ef000452987f1ce5b9cd7519
TamerUzgun <168527114+TamerUzgun@users.noreply.github.com>
2024-06-24 15:50:43 +0300

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Stock(models.Model):
-    symbol = models.CharField(max_length=10)
-    quantity = models.FloatField()
-    purchase_price = models.FloatField()
-
-    def __str__(self):
-        return self.symbol
